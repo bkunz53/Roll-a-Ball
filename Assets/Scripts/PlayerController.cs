ca28a7638f15a3b1a6e24adc1271c372b1586efe
bkunz53 <43080736+bkunz53@users.noreply.github.com>
2018-09-13 15:58:49 -0400

    Can pick up "Pick Ups"
    gained the ability to pick up "pick ups"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	
-	public float speed;
+    public float speed;
 
-	private Rigidbody rb;
-	void Start()
-	{
-		rb=GetComponent<Rigidbody>();
-	}
-	void FixedUpdate ()
-	{
-		float moveHorizontal=Input.GetAxis ("Horizontal");
-		float moveVertical=Input.GetAxis ("Vertical");
-		
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+    private Rigidbody rb;
 
-		rb.AddForce (movement*speed);
-	}
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ("Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+        }
+    }
 }
